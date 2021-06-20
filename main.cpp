a93595be885fadcfcb396ec682777f0e4d2f5f53
JBeamguard <jamie.beamguard@gmail.com>
2021-06-20 13:56:17 -0600

    Updating main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
+// File contains the main function for the overall program
+//	main function is based off a state machine topology using an infinite loop
+//	until user requests to exit the program.
+// Author: Jamie Beamguard
+// Last Revision: 6/20/2021
+
 #include <iostream>
+#include "menuHelpers.h"
+
 using namespace std;
+using namespace mainMenu;
 
 int main() {
-	string usInput;
+	int selection = -1;
 
-	cout << "Enter a word: " << endl;
-	cin >> usInput;
-	cout << "You wrote: " << endl << usInput << endl;
+	while(1) {
+		char userInput;
+
+		menuPrintHelper();		// print the menu
+
+		// get the user's input and react accordingly
+		cin >> userInput;
+		selection = menuSelectorHelper(userInput);
+
+		if(!selection) return 0;	// Exit program is chosen
+	}
 
 	return 0;
 }
