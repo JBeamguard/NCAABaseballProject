b6902965049c1b6c13c5cff11c4bb94c7d11b62d
JBeamguard <jamie.beamguard@gmail.com>
2021-06-19 20:49:17 -0600

    Final commit of the day

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-using namespace std
+using namespace std;
 
-void main() {
-	cout << "Hello World!" << endl;
+int main() {
+	string usInput;
+
+	cout << "Enter a word: " << endl;
+	cin >> usInput;
+	cout << "You wrote: " << endl << usInput << endl;
 
 	return 0;
 }
